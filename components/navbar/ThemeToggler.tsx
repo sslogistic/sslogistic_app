131b00de60a89a951915b14e5e82449c6a1b2990
Jane Moroz <Moroz.jane@yahoo.com>
2024-06-13 10:51:26 +0300

    fix: fix theme toggler

--- components/navbar/ThemeToggler.tsx
+++ components/navbar/ThemeToggler.tsx
@@ -7,7 +7,7 @@ import { Moon, Sun } from "@/assets/icons";
 
 const ThemeToggler = () => {
   const [mounted, setMounted] = useState(false);
-  const { systemTheme, theme, setTheme } = useTheme();
+  const { setTheme, theme, resolvedTheme } = useTheme();
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
@@ -18,10 +18,20 @@ const ThemeToggler = () => {
     return null;
   }
 
+  // If enableSystem is true and the active theme is "system" (when a user firsts visits a website), resolvedTheme returns whether the system preference resolved to "dark" or "light".
+  // So we need to set theme to resolvedTheme first time the user visits, but next time the theme is gonna come from localStorage
+  if (theme === "system" && resolvedTheme) {
+    setTheme(resolvedTheme);
+  }
+
+  const toggleTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
+
   return (
     <button
       aria-label="theme toggler"
-      onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+      onClick={toggleTheme}
       className="p-4 mx-2 sm:mx-4 md:my-2"
     >
       {theme === "dark" ? <Sun /> : <Moon />}
